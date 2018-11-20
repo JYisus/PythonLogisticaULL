bd87ede73b33106b29bcab3eabd1c38610ff9fd0
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-20 12:41:04 +0000

    TSP con u's

---
+++ tsp.py
@@ -0,0 +1,44 @@
+from ortools.linear_solver import pywraplp
+import random as rand
+
+solver = pywraplp.Solver('TSP', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+
+# x and y are integer non-negative variables.
+nodes = 25
+cost = []
+rand.seed(42)
+for i in range(nodes):
+    row = []
+    for j in range(nodes):
+        row.append(rand.randint(10,100))
+    cost.append(row)
+for i in range(nodes):
+    cost[i][i] = 0
+#print(cost)
+# cost = [[0,10,15,20],
+#    [10,0,35,25],
+#    [15,35,0,30],
+#    [20,25,30,0]]
+x = {}
+for i in range(nodes):
+    for j in range(nodes):
+        x[i, j] = solver.BoolVar('x[%i, %i]' % (i, j))
+u = {}
+for i in range(1,nodes):
+    u[i] = solver.IntVar(1.0,solver.infinity(),'u[%i]' % (i))
+
+solver.Minimize(solver.Sum([cost[i][j]*x[i,j] for i in range(nodes) for j in range(nodes)]))
+
+for i in range(nodes):
+    solver.Add(solver.Sum([x[i,j] for j in range(nodes)]) == 1)
+
+for j in range(nodes):
+    solver.Add(solver.Sum([x[i,j] for i in range(nodes)]) == 1)
+
+for i in range(1,nodes):
+    for j in range(1,nodes):
+        solver.Add(u[j]>=(u[i]+x[i,j]-(nodes-2)*(1-x[i,j])))
+
+sol = solver.Solve()
+if sol == solver.OPTIMAL:
+        print('Costo total =', solver.Objective().Value())
