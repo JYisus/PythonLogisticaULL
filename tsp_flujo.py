50636503f2968c74f179f0b9c92572395c70f6c0
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-12-04 11:32:00 +0000

    implementado modelo del TSP con flujos

--- tsp_flujo.py
+++ tsp_flujo.py
@@ -14,7 +14,7 @@ for i in range(nodes):
     cost.append(row)
 for i in range(nodes):
     cost[i][i] = 200
-print(cost)
+# print(cost)
 # cost = [[0,10,15,20],
 #    [10,0,35,25],
 #    [15,35,0,30],
@@ -24,8 +24,8 @@ for i in range(nodes):
     for j in range(nodes):
         x[i, j] = solver.BoolVar('x[%i, %i]' % (i, j))
 f = {}
-for i in range(0,nodes):
-    for j in range(0,nodes):
+for i in range(nodes):
+    for j in range(nodes):
         f[i, j] = solver.IntVar(0.0,solver.infinity(),'f[%i, %i]' % (i, j))
 
 solver.Minimize(solver.Sum([cost[i][j]*x[i,j] for i in range(nodes) for j in range(nodes)]))
@@ -36,12 +36,25 @@ for i in range(nodes):
 for j in range(nodes):
     solver.Add(solver.Sum([x[i,j] for i in range(nodes)]) == 1)
 
-for i in range(nodes):
+for i in range(1,nodes):
     solver.Add((solver.Sum([f[i,j] for j in range(nodes)])-solver.Sum([f[j,i] for j in range(nodes)]))==1)
-#solver.Add(solver.Sum(f[i,j] for i in range(1,nodes) for j in range(1,nodes))-solver.Sum[j,i] for i in range(1,nodes) for j in range(1,nodes))
+    #solver.Add(solver.Sum(f[i,j] for i in range(1,nodes) for j in range(1,nodes))-solver.Sum[j,i] for i in range(1,nodes) for j in range(1,nodes))
 for i in range(nodes):
     for j in range(nodes):
         solver.Add(0<=f[i,j]<=(nodes-1)*x[i,j])
+
 sol = solver.Solve()
 if sol == solver.OPTIMAL:
         print('Costo total =', solver.Objective().Value())
+        recorrido = '0'
+        i=0
+        while i != -1:
+            for j in range(nodes):
+                if x[i, j].solution_value() > 0:
+                    recorrido += ' -c(' + str(cost[i][j]) +')-> ' + str(j)
+                    aux = j
+            if aux != 0:
+                i = aux
+            else:
+                i = -1
+        print(recorrido)
